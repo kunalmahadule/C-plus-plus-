5c5f074e9cc08d069540dadf55991c08087dba1f
Kunal Mahadule <110099674+kunalmahadule@users.noreply.github.com>
2023-07-25 08:08:06 +0530

    Exercise on Inhertance

---
+++ tut42.cpp
@@ -0,0 +1,20 @@
+// Exercise on Inhertance
+
+#include <iostream>
+using namespace std;
+/*
+    Create 2 classes:
+        1. SimpleCalculator - Takes input of 2 numbers using a utility function and performs +, -, *, / and displays the result using another function.
+        2. ScientificCalculator - Takes input of 2 numbers using a utility function and performs any more scientific operation of your choice and displays the result using another function.
+
+        Create another class HybridCalculator and inherit it using these 2 classes:
+        Q1. What type of inheritance are you using?-->Multiple inheritance
+        Q2. Which mode of inheritance are you using?--> class HybridCalculator : public SimpleCalculator, public ScientificCalculator
+        Q3. Creat an object of HybridCalculator and display results of single and scientific calculator.
+        Q4. How is code reusability implemented?
+*/
+int main()
+{
+
+    return 0;
+}
