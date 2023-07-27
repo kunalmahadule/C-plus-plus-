b6067df833f0b0bc37863cc7dd8a9615772ad3bb
Kunal Mahadule <110099674+kunalmahadule@users.noreply.github.com>
2023-07-27 08:37:01 +0530

    Virtual Function Example + Creation Rules

---
+++ tut57.cpp
@@ -0,0 +1,82 @@
+// Virtual Function Example + Creation Rules
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+class CWH
+{
+protected:
+    string title;
+    float rating;
+
+public:
+    CWH(string s, float r)
+    {
+        title = s;
+        rating = r;
+    }
+    // virtual void display() {cout<<"bogus code";}-->Agar derived class me function nahi honge to base class ke call honge
+    virtual void display() {}
+};
+
+class CWHVideo : public CWH
+{
+    float videolength;
+
+public:
+    CWHVideo(string s, float r, float vl) : CWH(s, r)
+    {
+        videolength = vl;
+    }
+    void display()
+    {
+        cout << "This is an amazing video with title " << title << endl;
+        cout << "Ratings: " << rating << " out of 5 stars" << endl;
+        cout << "Length of this video is: " << videolength << " minutes" << endl;
+    }
+};
+
+class CWHText : public CWH
+{
+    int words;
+
+public:
+    CWHText(string s, float r, int wc) : CWH(s, r)
+    {
+        words = wc;
+    }
+    void display()
+    {
+        cout << "This is an amazing video with title " << title << endl;
+        cout << "Ratings: " << rating << " out of 5 stars" << endl;
+        cout << "No of words in this text tutorial is: " << words << " words" << endl;
+    }
+};
+
+int main()
+{
+    string title;
+    float rating, vlen;
+    int words;
+
+    // for Code With Harry Video
+    title = "Django tutorial";
+    vlen = 4.86;
+    rating = 4.98;
+    CWHVideo djVideo(title, rating, vlen);
+
+    // for Code With Harry Text
+    title = "Django tutorial Text";
+    words = 433;
+    rating = 4.19;
+    CWHText djText(title, rating, words);
+
+    CWH *tuts[2];
+    tuts[0] = &djVideo;
+    tuts[1] = &djText;
+
+    tuts[0]->display();
+    tuts[1]->display();
+
+    return 0;
+}
